654f3a92026b939c8b166e56b3c940014a93982d
Braulio Megias <braulio.megias@gmail.com>
2023-10-13 18:07:47 +0200

    some functions implemented

--- prj0/tictactoe/tictactoe.py
+++ prj0/tictactoe/tictactoe.py
@@ -23,8 +23,8 @@ def player(board):
     Returns player who has the next turn on a board.
     """
     if board[0].count(EMPTY)+board[1].count(EMPTY)+board[2].count(EMPTY) % 2 == 1 :
-        return X;
-    return O;
+        return X
+    return O
 
 def actions(board):
     """
@@ -44,22 +44,36 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    if is_three_board(board,O):
+        return O
+    if is_three_board(board,X):
+        return X
+    return None
 
+def is_three_board(board, player):
+    diag1 = [board[0][0],board[1][1],board[2][2]]
+    diag2 = [board[0][2],board[1][1],board[2][0]]
+    return any(is_three_row(row,player) for row in board) or is_three_row(diag1,player) or is_three_row(diag2,player) 
+
+def is_three_row(row, player):
+    return row.count(player) == 3
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    return winner(board) != None or not(any(r.count(EMPTY)>0 for r in board))
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
-
+    if winner(board) == X:
+        return 1
+    if winner(board) == O:
+        return -1
+    return 0
 
 def minimax(board):
     """
