d3aeba4f587d378c7c0424bc41e721b7c5feff6d
Braulio Megias <braulio.megias@gmail.com>
2023-11-01 22:04:57 +0100

    prepare to submit, restore original main function

--- prj4/shopping/shopping.py
+++ prj4/shopping/shopping.py
@@ -9,15 +9,14 @@ TEST_SIZE = 0.4
 
 
 def main():
-    """
+    
     # Check command-line arguments
     if len(sys.argv) != 2:
         sys.exit("Usage: python shopping.py data")
 
     # Load data from spreadsheet and split into train and test sets
     evidence, labels = load_data(sys.argv[1])
-    """
-    evidence, labels = load_data("shopping.csv")
+    
     X_train, X_test, y_train, y_test = train_test_split(
         evidence, labels, test_size=TEST_SIZE
     )
