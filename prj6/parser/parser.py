5b04e3205f39fb4afb0f38437c4a544cb3b94c5b
Braulio Megias <braulio.megias@gmail.com>
2023-11-09 20:49:18 +0100

    implemented preprocess

--- prj6/parser/parser.py
+++ prj6/parser/parser.py
@@ -62,7 +62,9 @@ def preprocess(sentence):
     and removing any word that does not contain at least one alphabetic
     character.
     """
-    raise NotImplementedError
+    words = [word.lower() for word in nltk.word_tokenize(sentence) if any(c.isalpha() for c in word)]
+    return words
+    #raise NotImplementedError
 
 
 def np_chunk(tree):
